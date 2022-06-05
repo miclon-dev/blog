6ea0b3202146ef34402b8095bfabda867bdff996
miclon <jcnd@163.com>
2022-06-05 17:10:07 +0800

    first commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: 'VuePress-Demo',
+    description: 'VuePress is a Vue-powered static site generator',
+})
