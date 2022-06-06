d0998e66cc4f04cedf9aa8a45ead618a490f50f9
miclon <jcnd@163.com>
2022-06-06 16:37:46 +0800

    1

--- .vuepress/navbar.ts
+++ .vuepress/navbar.ts
@@ -1,7 +1,11 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  "/",
+  {
+    text: "首页",
+    link: "/",
+    icon: "home"
+  },
   {
     text: "学习笔记",
     icon: "article",
