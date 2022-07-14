5c6d63e562f20eb77a1f188130665d6a2d5209e7
miclon <jcnd@163.com>
2022-07-14 13:01:29 +0800

    add posts

---
+++ .vuepress/components/my-directive.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+const vMyShow = {
+  mounted(el, binding) {
+    el.style.display = binding.value ? null : 'none'
+  },
+  updated(el, binding) {
+    el.style.display = binding.value ? null : 'none'
+  }
+}
+const flag = ref(false)
+</script>
+<template>
+  <button @click="flag=!flag">flag: {{flag}}</button>
+  <button v-my-show="flag">我是按钮，我不可见</button>
+</template>
