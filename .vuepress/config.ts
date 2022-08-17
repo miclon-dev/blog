fe2057f266511b26113c5d5e7154641e9897f3e8
miclon <jcnd@163.com>
2022-08-17 20:27:03 +0800

    baidu

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -10,6 +10,20 @@ export default defineUserConfig({
 
   base: "/",
 
+  head: [
+    [
+    'script',
+    {},
+    `var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?4845d5bae0cf1fabe802fdb0fe6dfc0b";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();`
+    ]
+  ],
+
   theme,
   plugins: [
     registerComponentsPlugin({
